faec383d5e114db3ea41d494bca50121c8bc5ee3
Tony Jackson <tony.jackson@srt-marine.com>
2024-08-05 15:18:44 +0100

    Update for pymodbus 3.7.0 compatibility

--- LW3010EC.py
+++ LW3010EC.py
@@ -5,6 +5,7 @@
 from serial.tools.list_ports import comports
 from serial import Serial, PARITY_NONE, STOPBITS_ONE, EIGHTBITS
 from pymodbus.client import ModbusSerialClient
+from pymodbus import pdu
 from enum import Enum
 from time import sleep
 import os
@@ -28,7 +29,7 @@ class PSU:
     def __init__(self, com_port=None, slaveId=0x1, debug=False):
         self.debug = debug
         self.com_port = self.find_PSU_com_port( com_port )
-        self.pymc = ModbusSerialClient(method='rtu', port=self.com_port, baudrate=9600, timeout=5)
+        self.pymc = ModbusSerialClient(port=self.com_port, baudrate=9600, timeout=5)
         self.slaveId = slaveId
 
     def find_PSU_com_port(self, com_port=None):
@@ -78,6 +79,11 @@ class PSU:
     def read(self, address, len=1):
         rr = self.pymc.read_holding_registers(address.value, len, slave=self.slaveId)
 
+        if not type(rr) is pdu.register_read_message.ReadHoldingRegistersResponse:
+            if self.debug:
+                print(address.name, rr)
+            return None
+
         if rr.isError():
             if self.debug:
                 print(address.name, rr.message)
@@ -87,7 +93,11 @@ class PSU:
 
     @property
     def current(self):
-        return self.read(PSU.Registers.CURRENT_READ)/100
+        current = self.read(PSU.Registers.CURRENT_READ)
+        if current is None:
+            return None
+        else:
+            return current/100
 
     @current.setter
     def current(self, amps):
@@ -95,7 +105,11 @@ class PSU:
 
     @property
     def voltage(self):
-        return self.read(PSU.Registers.VOLTAGE_READ)/100
+        voltage = self.read(PSU.Registers.VOLTAGE_READ)
+        if voltage is None:
+            return None
+        else:
+            return voltage/100
 
     @voltage.setter
     def voltage(self, volts):
@@ -119,13 +133,23 @@ class PSU:
 @click.option('--debug', is_flag=True, default=False, help='Show internal debug messages')
 @click.option('--com-port', type=str, default=None, help='Select com port to use. If omitted will self-detect PSU')
 @click.option('--slave-id', type=int, default=1, help='For dual-bank PSUs, select slave to control EG 1 (default) or 2')
-def psu_cmd(status, on, off, voltage, current, delay_on, debug, com_port, slave_id):
+@click.option('--scan', is_flag=True, default=False, help='Scan registers for non-zero values')
+def psu_cmd(status, on, off, voltage, current, delay_on, debug, com_port, slave_id, scan):
     """
     PSU controller
     
     If both --on and --off are specified, the output will be switched off first, other changes applied and then the output switched on.
     """
     psu = PSU(com_port=com_port, slaveId=slave_id, debug=debug)
+
+    if scan:
+        for address in range(0x1001,0x1100):
+            class test(Enum):
+                REGISTER = address
+            result = psu.read(test.REGISTER)
+            if(result > 0):
+                print('Address', address,':',result)
+        exit(0)
 
     if off:
         psu.output = False
